08884cccc17ff0d058a20d44e93087a8b00dbe4d
trapcodeio <trapcode.io@gmail.com>
2024-08-08 22:35:53 +0100

    refactor and import:logos

--- backend/jobs/cron.ts
+++ backend/jobs/cron.ts
@@ -2,7 +2,7 @@ import { CronTime } from "cron-time-generator";
 
 export default [
     {
-        job: "update:prices",
+        job: "update-prices",
         schedule: CronTime.every(2).minutes()
     }
 ];
